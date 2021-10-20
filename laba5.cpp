3bd72c5c4fc864155726da18f03caab4012a2a54
ViZEE18 <>
2021-10-20 17:32:43 +0300

    laba5

---
+++ laba5.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+bool IsLeapYear(int year)
+{
+    if (year < 0)
+        throw "Negative year";
+    if (year > 1000000)
+        throw "Future year";
+    return ((year % 4 == 0 && year % 100 != 0) || (year % 100 != 0 && year % 400 == 0));
+}
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    int year;
+    cout << "Введите год: ";
+    cin >> year;
+    try {
+        if (IsLeapYear(year))
+            cout << year << " этот год високосный!\n";
+        else
+            cout << year << " этот год не високосный!\n";
+    }
+    catch (char* i) {
+        cout << "Ошибка:" << i << endl;
+    }
+    system("pause");
+    return 0;;
+}
